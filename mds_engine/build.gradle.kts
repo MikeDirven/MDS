a35659074ff85ebac3549f7e6664ca0063e015f9
MikeDirven <m.dirven@ams.icsvertex.nl>
2023-10-16 16:18:37 +0200

    update 16/10/2023

---
+++ mds_engine/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    kotlin("multiplatform") version "1.9.0"
+    id("maven-publish")
+    application
+}
+
+val user: String = System.getenv("GITHUB_USER")
+val key: String = System.getenv("GITHUB_KEY")
+group = "nl.md-systems"
+version = "0.0.0.2"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation(project(mapOf("path" to ":mds_logging")))
+}
+
+publishing{
+    repositories {
+        maven {
+            name = "GitHub"
+            url = uri("https://maven.pkg.github.com/MikeDirven/MDS") // Github Package
+            credentials {
+                //Fetch these details from the properties file or from Environment variables
+                username = user
+                password = key
+            }
+        }
+    }
+}
+
+//dependencies {
+//    testImplementation(kotlin("test"))
+//    implementation("com.google.code.gson:gson:2.8.9")
+//}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvm() {
+        compilations.all {
+            kotlinOptions.jvmTarget = "17"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                api("ch.qos.logback:logback-classic:1.4.11")
+                api(kotlin("reflect"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+            }
+        }
+    }
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ mds_serialization_gson/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    kotlin("multiplatform") version "1.9.0"
+    id("maven-publish")
+}
+
+val user: String = System.getenv("GITHUB_USER")
+val key: String = System.getenv("GITHUB_KEY")
+group = "nl.md-systems"
+version = "0.0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+publishing{
+    repositories {
+        maven {
+            name = "GitHub"
+            url = uri("https://maven.pkg.github.com/MikeDirven/MDS") // Github Package
+            credentials {
+                //Fetch these details from the properties file or from Environment variables
+                username = user
+                password = key
+            }
+        }
+    }
+}
+
+kotlin {
+    jvm() {
+        compilations.all {
+            kotlinOptions.jvmTarget = "17"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(mapOf("path" to ":mds_engine")))
+                implementation(project(mapOf("path" to ":mds_serialization")))
+                api("com.google.code.gson:gson:2.8.9")
+                implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+            }
+        }
+    }
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
